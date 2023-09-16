a01263c37b181f5042d3fd76b2e89c01d941497b
cdmejiam94 <cdmejiam94@gmail.com>
2023-09-16 13:02:31 -0600

    Profile Fragment markup

--- app/src/main/java/com/test/dapokedex/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/test/dapokedex/ui/profile/ProfileFragment.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isEmpty
+import androidx.core.view.isNotEmpty
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.test.dapokedex.R
 import com.test.dapokedex.databinding.FragmentProfileBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -25,6 +29,32 @@ class ProfileFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            profileButton.setOnClickListener {
+
+                if (imageUrl.editText?.text?.length == 0 && imageName.editText?.text?.length != 0){
+                    cardImage.visibility = View.GONE
+                    cardName.visibility = View.VISIBLE
+                    var text = imageName.editText?.text
+                    var initials = text?.split(" ")
+
+                    cardName.setText(
+                        initials?.get(0)?.get(0)?.uppercase() +
+                                initials?.get(1)?.get(0)?.uppercase()
+                    )
+                } else if (imageUrl.editText?.text?.length != 0) {
+                    cardImage.visibility = View.VISIBLE
+                    cardName.visibility = View.GONE
+
+                    Glide.with(this@ProfileFragment)
+                        .load(imageUrl.editText?.text)
+                        .placeholder(R.drawable.avatar002)
+                        .dontAnimate()
+                        .into(cardImage)
+                }
+            }
+        }
     }
 
     companion object {
